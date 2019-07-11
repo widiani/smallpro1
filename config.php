01d063a0cc4030290a86a83fb2643b8ec28498ef
Widiani <widianicmh2001@gmail.com>
2019-07-11 09:55:47 +0700

    Commit Pertama

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "widi";
+$db_pass = "jaringan";
+$db_name = "dbsosmed";
+
+try {    
+    //create PDO connection 
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
+}
