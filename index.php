8ae5c5812a78ae06293467a96bd1d5c0d01b44b1
root <root@ip-172-16-2-49.us-east-2.compute.internal>
2019-07-18 04:41:00 +0000

    ubah title index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pesbuk</title>
+    <title>Pesbuk 2</title>
 
      <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
